6efd1c200b7a1aa5482dd7202b6da40d35fccda2
RoiBus19 <RoiBus19@htl-shkoder.com>
2026-03-28 17:01:41 +0100

    Commit
    Commit

--- Assets/Scripts/Save/FurniturePrefabReference.cs
+++ Assets/Scripts/Save/FurniturePrefabReference.cs
@@ -4,7 +4,7 @@
 /// This component stores the reference to the original prefab ScriptableObject
 /// so we can load the correct prefab when deserializing saved furniture
 /// </summary>
-public class FurniturePrefabReference : MonoBehaviour
+public partial class FurniturePrefabReference : MonoBehaviour
 {
     public string prefabPath; // The name of the InventoryItemData ScriptableObject
 }
